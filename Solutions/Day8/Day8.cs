09a113c537439ea27bc0815fc21cd2aa434e2d31
Kenny Eliasson <kenny.eliasson@bambora.com>
2022-12-08 07:48:02 +0100

    Solves Day 8

---
+++ Solutions/Day8/Day8.cs
@@ -0,0 +1,112 @@
+namespace Solutions;
+
+public class Day8
+{
+    private readonly ITestOutputHelper _output;
+
+    public Day8(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    private record Tree(int Height, int Row, int Column)
+    {
+        public bool AtTheEdge(int maxRows, int maxColumns)
+        {
+            return Row == 0 || Column == 0 || Row == maxRows - 1 || Column == maxColumns - 1;
+        }
+    }
+
+    [Fact]
+    public void PartOne()
+    {
+        var lines = File.ReadLines("Day8/input.txt").ToList();
+        
+        var columnCount = lines[0].Length;
+        var rowCount = lines.Count;
+        
+        var trees = CreateForest(lines);
+
+        var visible = 0;
+        foreach (var tree in trees)
+        {
+            if (tree.AtTheEdge(rowCount, columnCount))
+            {
+                visible++;
+                continue;
+            }
+            
+            var treesInSameColumn = trees.Where(x => x.Column == tree.Column).ToList();
+            var treesInSameRow = trees.Where(x => x.Row == tree.Row).ToList();
+
+            var down = treesInSameColumn.Where(x => x.Row > tree.Row).Any(x => x.Height >= tree.Height);
+            var up = treesInSameColumn.Where(x => x.Row < tree.Row).Any(x => x.Height >= tree.Height);
+                
+            var right = treesInSameRow.Where(x => x.Column > tree.Column).Any(x => x.Height >= tree.Height);
+            var left = treesInSameRow.Where(x => x.Column < tree.Column).Any(x => x.Height >= tree.Height);
+            
+            if (!down || !up || !right || !left)
+            {
+                visible++;
+            }
+        }
+        
+        _output.WriteLine($"Visible tree count is {visible}");
+    }
+
+    private static List<Tree> CreateForest(List<string> lines)
+    {
+        var trees = new List<Tree>();
+
+        foreach (var row in lines.Select((line, index) => (line, index)))
+        {
+            foreach (var column in row.line.Select((letter, index) => (letter, index)))
+            {
+                var treeHeight = int.Parse(column.letter.ToString());
+                var tree = new Tree(treeHeight, row.index, column.index);
+                trees.Add(tree);
+            }
+        }
+
+        return trees;
+    }
+
+    [Fact]
+    public void PartTwo()
+    {
+        var lines = File.ReadLines("Day8/input.txt").ToList();
+        
+        var columnCount = lines[0].Length;
+        var rowCount = lines.Count;
+        
+        var trees = CreateForest(lines);
+
+        var scenicScores = new List<int>();
+        foreach (var tree in trees.Where(tree => !tree.AtTheEdge(rowCount, columnCount)))
+        {
+            var down = trees.Where(x => x.Column == tree.Column && x.Row > tree.Row).ToList();
+            var up = trees.Where(x => x.Column == tree.Column && x.Row < tree.Row).Reverse().ToList();
+                
+            var right = trees.Where(x => x.Row == tree.Row && x.Column > tree.Column).ToList();
+            var left = trees.Where(x => x.Row == tree.Row && x.Column < tree.Column).Reverse().ToList();
+            
+            scenicScores.Add(CalculateScenicScore(down, tree) * CalculateScenicScore(up, tree) * CalculateScenicScore(right, tree) * CalculateScenicScore(left, tree));
+        }
+
+        _output.WriteLine($"Maximum scenic score is {scenicScores.Max()}");
+    }
+
+    private static int CalculateScenicScore(List<Tree> treeLine, Tree tree)
+    {
+        for (var i = 0; i < treeLine.Count; i++)
+        {
+            if (treeLine[i].Height >= tree.Height)
+            {
+                return i + 1;
+            }
+        }
+
+        return treeLine.Count;
+    }
+}
+
