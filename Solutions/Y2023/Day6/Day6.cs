ee325ad489a94fbed6e88de57fd7f4a9f169ed47
Kenny Eliasson <kenny@easyserv.se>
2023-12-08 06:59:50 +0100

    Day6 and half part1 day8

---
+++ Solutions/Y2023/Day6/Day6.cs
@@ -0,0 +1,57 @@
+namespace Solutions.Y2023;
+
+public class Day6 : AdventOfCodeTests
+{
+
+    public Day6(ITestOutputHelper output) : base(output)
+    {
+        
+    }
+
+
+
+    public override void PartOne(List<string> lines)
+    {
+        /*var races = new List<(int time, int distance)>()
+        {
+            (7, 9), (15,40), (30, 200)
+        };*/
+        
+        var races = new List<(int time, long distance)>()
+        {
+            (42899189, 308117012911467)
+        };
+        
+        var raceWins = new List<int>();
+        foreach (var race in races)
+        {
+            var wins = 0;
+            
+            for (long i = 0; i < race.time; i++)
+            {
+                var speed = i;
+                var left = race.time - i;
+
+                
+                long travel = speed * left;
+                if (travel > race.distance)
+                    wins++;
+               // _output.WriteLine($"Pushed for {speed} ({left}) and raced {travel} mm");
+            }
+            
+            raceWins.Add(wins);
+
+        }
+
+        
+        foreach (var win in raceWins)
+        {
+            
+        }
+        _output.WriteLine(string.Join(", ", raceWins));
+    }
+
+    public override void PartTwo(List<string> lines)
+    {
+    }
+}
---
+++ Solutions/Y2023/Day8/Day8.cs
@@ -0,0 +1,77 @@
+namespace Solutions.Y2023;
+
+public class Day8 : AdventOfCodeTests
+{
+
+    public Day8(ITestOutputHelper output) : base(output)
+    {
+        
+    }
+    
+
+    public override void PartOne(List<string> lines)
+    {
+        var instructions = lines[0].Select(x => x).ToList();
+        var elementInput = lines.Skip(2).Select(x => (x[..3], x[7..10], x[12..15])).ToDictionary(x => x.Item1);
+        
+        var steps = GoToZZZ(elementInput, instructions);
+        
+        _output.WriteLine($"Steps = {steps}");  
+    }
+
+    private static int GoToZZZ(Dictionary<string, (string, string, string)> elementInput, List<char> instructions)
+    {
+        var input = elementInput["AAA"];
+        int steps = 0;
+        while (true)
+        {
+            foreach (var instruction in instructions)
+            {
+                input = instruction == 'L' ? elementInput[input.Item2] : elementInput[input.Item3];
+                steps++;
+
+                if (input.Item1 == "ZZZ")
+                    return steps;
+            }
+        }
+    }
+
+    public override void PartTwo(List<string> lines)
+    {
+        var instructions = lines[0].Select(x => x).ToList();
+        var elementInput = lines.Skip(2).Select(x => (x[..3], x[7..10], x[12..15])).ToDictionary(x => x.Item1);
+        
+        
+        var steps = GoToZZZAsGhost(elementInput, instructions);
+        
+        _output.WriteLine($"Steps = {steps}");
+    }
+
+    private long GoToZZZAsGhost(Dictionary<string,(string, string, string)> elementInput, List<char> instructions)
+    {
+        var inputs = elementInput.Where(x => x.Key.EndsWith('A')).Select(x => x.Value).ToList();
+        int steps = 0;
+        while (true)
+        {
+            foreach (var instruction in instructions)
+            {
+                var newInput = new List<(string, string, string)>();
+
+                foreach (var input in inputs)
+                {
+                    var match = instruction == 'L' ? elementInput[input.Item2] : elementInput[input.Item3];
+                    newInput.Add(match);
+                }
+                
+                steps++;
+
+                if (newInput.All(x => x.Item1.EndsWith('Z')))
+                {
+                    return steps;
+                }
+
+                inputs = newInput;
+            }
+        }
+    }
+}
